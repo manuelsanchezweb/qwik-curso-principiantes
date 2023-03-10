7b03f1d5990610028c228f32cfdb7572d7894e7a
manuelsanchez2 <manusansan22@gmail.com>
2023-03-10 20:51:34 +0100

    🆕 feat: Create and finish branch

---
+++ src/types.d.ts
@@ -0,0 +1,61 @@
+export interface Character {
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  domain: string;
+  ip: string;
+  address: {
+    address: string;
+    city: string;
+    coordinates: {
+      lat: number;
+      lng: number;
+    };
+    postalCode: string;
+    state: string;
+  };
+  macAddress: string;
+  university: string;
+  bank: {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+  };
+  company: {
+    address: {
+      address: string;
+      city: string;
+      coordinates: {
+        lat: number;
+        lng: number;
+      };
+      postalCode: string;
+      state: string;
+    };
+    department: string;
+    name: string;
+    title: string;
+  };
+  ein: string;
+  ssn: string;
+  userAgent: string;
+}
