4e1c12a9154e008130ccabfeae1d5ea64a843fc5
manuelsanchez2 <manusansan22@gmail.com>
2023-03-16 15:43:59 +0100

    🆕 feat: Add content about components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+// export default component$(() => {
+//   return <div>Hello Qwik!</div>
+// });
+
+export const Header = component$(() => {
+  return (
+    <>
+      <div>Holaaaa</div>
+      <h1>This is the header</h1>
+    </>
+  );
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,30 +1,5 @@
-import { component$, Slot } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '../components/header/header';
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
-  return (
-    <>
-      <main>
-        <Header />
-        <section>
-          <Slot />
-        </section>
-      </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-          <div>{serverTime.value.date}</div>
-        </a>
-      </footer>
-    </>
-  );
+  return <Slot />;
 });
